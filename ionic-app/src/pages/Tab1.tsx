3fe987c0fad00d751cad038b51058b531beb7cd4
Lilic14991 <lilic14991@gmail.com>
2025-06-02 11:14:21 +0200

    test local notification

--- ionic-app/src/pages/Tab1.tsx
+++ ionic-app/src/pages/Tab1.tsx
@@ -5,7 +5,6 @@ import {
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import ExploreContainer from "../components/ExploreContainer";
 import DynamicIframe from "../components/DynamicIframe";
 import "./Tab1.css";
 import { LocalNotifications } from "@capacitor/local-notifications";
